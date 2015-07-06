c4757117299270319f7a9a3d5fcb060356bf196e
panindra Tumkur Seetharamu <ptumkurseetharamu@godaddy.com>
2015-07-06 09:59:11 -0700

    Integrated Social framework to post to Facebook and Twitter

--- SocialSharing/ViewController.swift
+++ SocialSharing/ViewController.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var opinionTextField: UITextField!
+
+    @IBOutlet weak var URLTextField: UITextField!
+    @IBOutlet weak var shareImageSwitch: UISwitch!
+
+    var postText: String = "Keep calm and feel like a sir"
+    var URLToShare: NSURL = NSURL(string: "udacity.com")!
+    let imageToShare: UIImage = UIImage(named: "LikeASir")!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func postToFacebook(sender: AnyObject) {
+        postToSocialMedia(SLServiceTypeFacebook)
+    }
+
+    @IBAction func postToTwitter(sender: AnyObject) {
+        postToSocialMedia(SLServiceTypeTwitter)
+    }
+
+    func postToSocialMedia(typeOfMedia : String) {
+        parseTextFields()
+        let socailPostViewController = SLComposeViewController(forServiceType: typeOfMedia)
+        socailPostViewController.setInitialText(postText)
+        socailPostViewController.addURL(URLToShare)
+
+        if(shareImageSwitch.on) {
+            socailPostViewController.addImage(imageToShare)
+        }
+        self.presentViewController(socailPostViewController, animated: true, completion: nil)
+    }
+
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    private func parseTextFields() {
+
+        if (!opinionTextField.text.isEmpty) {
+            postText = opinionTextField.text
+        }
+
+        if (!URLTextField.text!.isEmpty) {
+            URLToShare = NSURL(string: URLTextField.text!)!
+        }
+    }
 
 }
 
